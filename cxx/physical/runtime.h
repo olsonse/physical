f6d73e05ef1c855e52ae0bc4538af439cc4876db
Spencer E. Olson <olsonse@umich.edu>
2009-10-24 21:13:10 -0600

    added alias runtime.h header file for when using runtime::physical::*

---
+++ cxx/physical/runtime.h
@@ -0,0 +1,10 @@
+/** \file
+ * This file mostly provides an alias for importing <physical/quantity.h>.
+ * */
+
+#ifndef physical_runtime_h
+#define physical_runtime_h
+
+#include <physical/quantity.h>
+
+#endif // physical_runtime_h
