9d78d214933e3b5ca6bb894aae4bcf798b9f730c
Spencer E. Olson <olsonse@umich.edu>
2009-11-04 22:31:16 -0700

    added doxygen api and installation instructions

---
+++ docs/api/mainpage.h
@@ -0,0 +1,55 @@
+/** \file
+ * The main documentation page for doxygen. 
+ */
+
+/** \mainpage Physical Units and Constants
+
+
+        <h3>Technical Reference</h3>
+    - This is the beginning documentation for the Physical package.
+
+
+  For reference, please also see the following sections:
+  - \subpage license
+  - \subpage readme
+  - \subpage install
+  - \subpage changelog
+  - \subpage authors
+*/
+
+
+
+//-----------------------------------------------------------
+/** \page license Licence
+    \verbinclude COPYING
+*/
+
+
+
+//-----------------------------------------------------------
+/** \page readme README
+    \verbinclude README
+*/
+
+
+
+//-----------------------------------------------------------
+/** \page install INSTALL
+    \verbinclude INSTALL
+*/
+
+
+
+//-----------------------------------------------------------
+/** \page changelog ChangeLog
+    \verbinclude ChangeLog
+*/
+
+
+
+//-----------------------------------------------------------
+/** \page authors AUTHORS
+    \verbinclude AUTHORS
+*/
+
+
